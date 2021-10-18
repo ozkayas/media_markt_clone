088e3b522c44f6157849067b6743d2b1d868dd69
Suat Özkaya <suatozkaya@gmail.com>
2021-10-18 15:41:58 +0300

    Add webview package and details page

--- lib/views/product_detail_view.dart
+++ lib/views/product_detail_view.dart
@@ -1,7 +1,22 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ProductDetailView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ProductDetailView extends StatefulWidget {
   const ProductDetailView({Key? key}) : super(key: key);
+
+  @override
+  _ProductDetailViewState createState() => _ProductDetailViewState();
+}
+
+class _ProductDetailViewState extends State<ProductDetailView> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
 
   @override
   Widget build(BuildContext context) {
